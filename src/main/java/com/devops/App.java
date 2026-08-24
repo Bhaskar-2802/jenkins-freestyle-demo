8c05200e76bd9ca49279ae68ecd4f3765380cc3c
Bhaskar-2802 <bhaskar.unf207@gmail.com>
2026-08-24 03:03:50 -0400

    Add Java application

---
+++ src/main/java/com/devops/App.java
@@ -0,0 +1,12 @@
+package com.devops;
+
+public class App {
+
+    public static String message() {
+        return "Hello from DevOps!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(message());
+    }
+}
