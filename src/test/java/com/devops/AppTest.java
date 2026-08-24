8f0ce6e40a26e9c20d5ff7017fce495b2ce5d26b
Bhaskar-2802 <bhaskar.unf207@gmail.com>
2026-08-24 03:06:04 -0400

    Add unit test

---
+++ src/test/java/com/devops/AppTest.java
@@ -0,0 +1,13 @@
+package com.devops;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testMessage() {
+        assertEquals("Hello from DevOps!", App.message());
+    }
+}
